1f08ffca164054b2abd9eed85eeec49eeed2ebc5
imsakthipriya <116377287+imsakthipriya@users.noreply.github.com>
2022-10-23 15:09:22 +0530

    sunday 25-10-222

---
+++ my programs/basic/simple_calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    int x, y;
+    cin >> x;
+    cin >> y;
+    cout << x + y;
+    cout << "\n";
+
+    cout << x / y;
+    cout << "\n";
+
+    cout << x - y;
+    cout << "\n";
+
+    cout << x * y;
+    cout << "\n";
+}
