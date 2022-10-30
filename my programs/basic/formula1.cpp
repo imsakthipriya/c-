e6aeff687e28b866220d320cc15b6fe0d9d3d88b
S Hariprakash <shariprakash1805@users.noreply.github.com>
2022-10-31 00:25:07 +0530

    formula execution in cpp

---
+++ my programs/basic/formula1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << "a^2+b^2 = " << a * a + b * b << endl;
+    cout << "a^2-b^2 = " << a * a - b * b << endl;
+    cout << "a^2*b^2 = " << a * a * b * b;
+}
